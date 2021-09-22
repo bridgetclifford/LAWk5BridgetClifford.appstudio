320118603f79b6715098419f9edf959e968dc5fd
bridgetclifford <90580691+bridgetclifford@users.noreply.github.com>
2021-09-22 14:40:27 -0500

    strings

---
+++ forms/strings/strings.js
@@ -0,0 +1,17 @@
+let quoteString = "Failure is simply the opportunity to begin again this time more intelligently."
+
+quoteString = quoteString.toUpperCase()
+console.log(`Upper case string is: ${quoteString}`)
+
+let authorString = "- Henry Ford"
+
+let completeString = quoteString.concat(authorString)
+console.log(`Concatenated string is: ${completeString}`)
+
+let found = completeString.includes("Henry")
+if (found == true) {
+  console.log(`Henry was in complete.String.`)
+}
+else {
+  console.log(`Henry was NOT in complete.String.`)
+}
